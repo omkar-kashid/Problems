48162e619b9fa400026f90eed23392bca80a5e43
Omkar Kashid <108173466+omkar-kashid@users.noreply.github.com>
2023-06-28 15:48:57 +0530

    Added solution - LeetHub

--- Koko Eating Bananas - GFG/koko-eating-bananas.cpp
+++ Koko Eating Bananas - GFG/koko-eating-bananas.cpp
@@ -9,12 +9,7 @@ class Solution {
   int requiredTime(vector<int> &piles, int hours){
       int ans=0;
       for(int i=0; i<piles.size(); i++){
-          if(piles[i]%hours==0){
-              ans += (piles[i]/hours);
-          }
-          else{
-              ans += (piles[i]/hours)+1;
-          }
+          ans += ceil((double)piles[i]/(double)hours);
       }
       return ans;
   }
@@ -25,19 +20,17 @@ class Solution {
         for(int i=0; i<N; i++){
             maxi = max(maxi,piles[i]);
         }
-        int ans;
         int i=1, j=maxi;
         while(i<=j){
             int mid = (i+j)/2;
-            if(requiredTime(piles, mid)<=H){
-                ans = mid;
+            if(requiredTime(piles, mid)<=H)
                 j = mid -1;
-            }
-            else if(requiredTime(piles,mid)>H){
+        
+            else
                 i = mid + 1;
-            }
+            
         }
-        return ans;
+        return i;
     }
 };
 
