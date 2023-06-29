706a3cd37d2eb7d0935140e3113cbe8e4cde8b20
Omkar Kashid <108173466+omkar-kashid@users.noreply.github.com>
2023-06-29 12:31:25 +0530

    Added solution - LeetHub

--- Capacity To Ship Packages Within D Days - GFG/capacity-to-ship-packages-within-d-days.cpp
+++ Capacity To Ship Packages Within D Days - GFG/capacity-to-ship-packages-within-d-days.cpp
@@ -15,9 +15,11 @@ class Solution {
         for(int i=0; i<n; i++){
             if(currentWeight+arr[i] > cap){
                 TotalDaysRequired++;
-                currentWeight = 0;
+                currentWeight = arr[i];
             }
-            currentWeight+=arr[i];
+            else{
+                currentWeight += arr[i];
+            }
         }
         if(TotalDaysRequired>day) return false;
         return true;
@@ -25,17 +27,24 @@ class Solution {
     int leastWeightCapacity(int arr[], int N, int D) {
         // code here
         int maxi = INT_MIN;
-        long long sum = 0;
+        int sum = 0;
         for(int i=0; i<N; i++){
             maxi = max(maxi,arr[i]);
             sum += arr[i];
         }
-        for(int i=maxi; i<=sum; i++){
-            if(capacity(arr,D,i, N)){
-                return i;
+        int i = maxi;
+        int j = sum;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(capacity(arr,D,mid,N)){
+                j = mid - 1;
             }
+            else{
+                i = mid + 1;
+            }
+            
         }
-        return -1;
+        return i;
     }
 };
 
