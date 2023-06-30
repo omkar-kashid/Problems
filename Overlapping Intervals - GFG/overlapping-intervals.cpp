296f6c25af9493f75a8a4b7b7569059358eadef0
Omkar Kashid <108173466+omkar-kashid@users.noreply.github.com>
2023-06-30 15:18:41 +0530

    Added solution - LeetHub

--- Overlapping Intervals - GFG/overlapping-intervals.cpp
+++ Overlapping Intervals - GFG/overlapping-intervals.cpp
@@ -11,20 +11,12 @@ public:
          int n = intervals.size();
          vector<vector<int>> ans;
          for(int i=0; i<n; i++){
-             int start = intervals[i][0];
-             int end = intervals[i][1];
-             if(!ans.empty() && ans.back()[1]>=end){
-                 continue;
+             if(ans.empty() || ans.back()[1]<intervals[i][0]){
+                 ans.push_back(intervals[i]);
              }
-             for(int j= i+1; j<n; j++){
-                 if(intervals[j][0]<=end){
-                     end = max(end, intervals[j][1]);
-                 }
-                 else{
-                     break;
-                 }
+             else{
+                 ans.back()[1] = max(ans.back()[1],intervals[i][1]);
              }
-             ans.push_back({start,end});
          }
          return ans;
     }
