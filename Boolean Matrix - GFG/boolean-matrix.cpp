56c3868ebe8f0aa6c58f5f8309dbf9eb3569e9c3
Omkar Kashid <108173466+omkar-kashid@users.noreply.github.com>
2023-09-30 09:32:45 +0530

    Added solution - LeetHub

--- Boolean Matrix - GFG/boolean-matrix.cpp
+++ Boolean Matrix - GFG/boolean-matrix.cpp
@@ -13,49 +13,31 @@ class Solution
     void booleanMatrix(vector<vector<int> > &matrix)
     {
         // code here 
-        int n= matrix.size();
-        int m = matrix[0].size();
-        int col0 = matrix[0][0];
-    // step 1: Traverse the matrix and
-    // mark 1st row & col accordingly:
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == 1) {
-                // mark i-th row:
-                matrix[i][0] = 1;
-
-                // mark j-th column:
-                if (j != 0)
-                    matrix[0][j] = 1;
-                else
-                    col0 = 1;
-            }
-        }
-    }
-
-    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < m; j++) {
-            if (matrix[i][j] != 1) {
-                // check for col & row:
-                if (matrix[i][0] == 1 || matrix[0][j] == 1) {
-                    matrix[i][j] = 1;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]==1){
+                    for(int k=0; k<n; k++){
+                        if(matrix[k][j]!=1){
+                            matrix[k][j]=-1;
+                        }
+                    }
+                    for(int l=0; l<m; l++){
+                        if(matrix[i][l]!=1){
+                            matrix[i][l]=-1;
+                        }
+                    }
                 }
             }
         }
-    }
-
-    //step 3: Finally mark the 1st col & then 1st row:
-    if (matrix[0][0] == 1) {
-        for (int j = 0; j < m; j++) {
-            matrix[0][j] = 1;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]==-1) 
+                    matrix[i][j]=1;
+            }
         }
-    }
-    if (col0 == 1) {
-        for (int i = 0; i < n; i++) {
-            matrix[i][0] = 1;
-        }
-    }
     }
 };
 
